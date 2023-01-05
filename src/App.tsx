8e8045ad4263f7d43b2798efa889094524d0eb9a
abc <abc@test.com>
2023-01-06 02:56:46 +0800

    ant designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import './login.css';
 
-function App() {
+const App: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div> hello world</div>
+    <div className="login-page">
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 24 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      
+
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password prefix={<LockOutlined className="site-form-item-icon" />}  placeholder='password'/>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 9, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          login
+        </Button>
+      </Form.Item>
+    </Form>
+    </div>
   );
-}
+};
 
 export default App;
